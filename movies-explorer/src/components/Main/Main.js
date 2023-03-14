8409f2a441111c9e6fa81de590787740f9951c62
MariaMantusova <maria.mantusova@mail.ru>
2023-03-14 16:20:34 +0300

    feat: add footer

--- movies-explorer/src/components/Main/Main.js
+++ movies-explorer/src/components/Main/Main.js
@@ -6,6 +6,7 @@ import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 
 function Main() {
     return (
@@ -23,6 +24,7 @@ function Main() {
                 <AboutMe/>
                 <Portfolio/>
             </main>
+            <Footer/>
         </>
     )
 }
