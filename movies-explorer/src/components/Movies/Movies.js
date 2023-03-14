ad07b954d2634d1e6f395d8384b2f710d9c16ac1
MariaMantusova <maria.mantusova@mail.ru>
2023-03-14 21:24:45 +0300

    feat: add navlink

--- movies-explorer/src/components/Movies/Movies.js
+++ movies-explorer/src/components/Movies/Movies.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import "./Movies.css";
 import Header from "../Header/Header";
 import iconProfile from "../../images/icon-profile.png";
@@ -9,14 +9,18 @@ function Movies() {
         <>
             <Header>
                 <div className="header__links-container">
-                    <Link to="/movies" className="header__nav-link">Фильмы</Link>
-                    <Link to="/saved-movies" className="header__nav-link">Сохранённые фильмы</Link>
-                    <Link to="/profile" className="header__nav-link">
+                    <NavLink to="/movies"
+                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Фильмы</NavLink>
+                    <NavLink to="/saved-movies"
+                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Сохранённые
+                        фильмы</NavLink>
+                    <NavLink to="/profile"
+                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>
                         <p className="header__nav-link-text">Аккаунт</p>
                         <div className="header__nav-link-icon-background">
                             <img className="header__nav-link-icon" src={iconProfile} alt="Иконка профиля"/>
                         </div>
-                    </Link>
+                    </NavLink>
                 </div>
             </Header>
         </>
