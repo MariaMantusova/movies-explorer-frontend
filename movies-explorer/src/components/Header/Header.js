3a12c4be9c7cea3e971176bccba9ca84cb7b429c
MariaMantusova <maria.mantusova@mail.ru>
2023-03-16 17:16:58 +0300

    feat: add header

--- movies-explorer/src/components/Header/Header.js
+++ movies-explorer/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header.css";
+import {Link} from "react-router-dom";
 import logoImage from "../../images/logo.svg";
 
 function Header(props) {
     return(
         <header className="header">
-            <img className="header__logo" src={logoImage} alt="logo"/>
+            <Link to="/">
+                <img className="header__logo" src={logoImage} alt="logo"/>
+            </Link>
             {props.children}
         </header>
     )
