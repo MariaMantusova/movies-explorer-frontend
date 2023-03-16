f965e3c06bdb069b425d36ea9bd2eacd7c588613
MariaMantusova <maria.mantusova@mail.ru>
2023-03-16 07:49:47 +0300

    feat: add profile

--- movies-explorer/src/components/Profile/Profile.js
+++ movies-explorer/src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Profile.css";
+import {Link} from "react-router-dom";
 import Header from "../Header/Header";
 import {NavLink} from "react-router-dom";
 import iconProfile from "../../images/icon-profile.svg";
@@ -23,6 +24,21 @@ function Profile() {
                     </NavLink>
                 </div>
             </Header>
+            <section className="profile">
+                <h1 className="profile__header">Привет, Мария!</h1>
+                <form className="profile__form">
+                    <div className="profile__input-container profile__input-container_type_top">
+                        <label className="profile__label">Имя</label>
+                        <input className="profile__input" type="text" defaultValue="Мария"/>
+                    </div>
+                    <div className="profile__input-container profile__input-container_type_bottom">
+                        <label className="profile__label">E-mail</label>
+                        <input className="profile__input" type="email" defaultValue="pochta@yandex.ru"/>
+                    </div>
+                    <button className="profile__button">Редактировать</button>
+                </form>
+                <Link to="/" className="profile__link">Выйти из аккаунта</Link>
+            </section>
         </>
     )
 }
