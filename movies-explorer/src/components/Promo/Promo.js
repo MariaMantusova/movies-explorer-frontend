5a9c37a5a526c731a82dda01b57b73d2cb26a281
MariaMantusova <maria.mantusova@mail.ru>
2023-03-16 20:09:52 +0300

    feat: add mobile adaptive

--- movies-explorer/src/components/Promo/Promo.js
+++ movies-explorer/src/components/Promo/Promo.js
@@ -5,7 +5,7 @@ import "./Promo.css";
 function Promo(props) {
     return(
         <section className="promo">
-            <h1 className="promo__header">Учебный проект студента факультета Веб-разработки.</h1>
+            <h1 className="promo__header">Учебный проект студента&nbsp;факультета Веб-разработки.</h1>
             <NavTab onClickAboutProject={props.onClickAboutProject} onClickTechs={props.onClickTechs}
                     onClickStudent={props.onClickStudent} />
         </section>
