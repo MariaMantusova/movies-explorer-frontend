fad9b7285b95f53fe11789aa45061063c0174804
MariaMantusova <maria.mantusova@mail.ru>
2023-03-17 19:22:18 +0300

    feat: add headerlinks component

--- movies-explorer/src/components/Movies/Movies.js
+++ movies-explorer/src/components/Movies/Movies.js
@@ -1,31 +1,17 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
 import "./Movies.css";
 import Header from "../Header/Header";
-import iconProfile from "../../images/icon-profile.svg";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Preloader from "../Preloader/Preloader";
 import Footer from "../Footer/Footer";
+import HeaderLinks from "../Header/HeaderLinks";
 
 function Movies() {
     return(
         <>
             <Header>
-                <div className="header__links-container">
-                    <NavLink to="/movies"
-                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Фильмы</NavLink>
-                    <NavLink to="/saved-movies"
-                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Сохранённые
-                        фильмы</NavLink>
-                    <NavLink to="/profile"
-                          className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>
-                        <p className="header__nav-link-text">Аккаунт</p>
-                        <div className="header__nav-link-icon-background">
-                            <img className="header__nav-link-icon" src={iconProfile} alt="Иконка профиля"/>
-                        </div>
-                    </NavLink>
-                </div>
+                <HeaderLinks/>
             </Header>
             <SearchForm/>
             <MoviesCardList class="movie__save-button_active"/>
--- movies-explorer/src/components/Profile/Profile.js
+++ movies-explorer/src/components/Profile/Profile.js
@@ -2,27 +2,13 @@ import React from "react";
 import "./Profile.css";
 import {Link} from "react-router-dom";
 import Header from "../Header/Header";
-import {NavLink} from "react-router-dom";
-import iconProfile from "../../images/icon-profile.svg";
+import HeaderLinks from "../Header/HeaderLinks";
 
 function Profile() {
-    return(
+    return (
         <>
             <Header>
-                <div className="header__links-container">
-                    <NavLink to="/movies"
-                             className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Фильмы</NavLink>
-                    <NavLink to="/saved-movies"
-                             className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>Сохранённые
-                        фильмы</NavLink>
-                    <NavLink to="/profile"
-                             className={({isActive}) => `header__nav-link ${isActive ? "header__nav-link_active" : ""}`}>
-                        <p className="header__nav-link-text">Аккаунт</p>
-                        <div className="header__nav-link-icon-background">
-                            <img className="header__nav-link-icon" src={iconProfile} alt="Иконка профиля"/>
-                        </div>
-                    </NavLink>
-                </div>
+                <HeaderLinks/>
             </Header>
             <section className="profile">
                 <h1 className="profile__header">Привет, Мария!</h1>
