c1698cff2f5ce92d35730f13d82278c9af9d7109
MariaMantusova <maria.mantusova@mail.ru>
2023-03-30 17:31:54 +0300

    feat: add preloadr component

--- movies-explorer/src/components/Preloader/Preloader.js
+++ movies-explorer/src/components/Preloader/Preloader.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Preloader.css";
+import preloader from "../../images/preloader.svg";
 
 function Preloader() {
     return(
-        <section className="preloader">
-            <button className="preloader__button">Ещё</button>
+        <section className="preloader-page">
+            <img className="preloader" src={preloader} alt="Загрузка страницы"/>
         </section>
     )
 }
