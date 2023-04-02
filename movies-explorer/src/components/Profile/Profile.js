1397874bdb762427017baa44802b797b7f35d79f
MariaMantusova <maria.mantusova@mail.ru>
2023-04-02 15:01:52 +0300

    feat: add profile validation

--- movies-explorer/src/components/Profile/Profile.js
+++ movies-explorer/src/components/Profile/Profile.js
@@ -5,24 +5,37 @@ import "../Header/Header.css";
 import HeaderToMobile from "../Header/HeaderToMobile";
 import HeaderBurger from "../Header/HeaderBurger";
 import {CurrentUserContext} from "../../contexts/CurrentUserContext";
+import {useInput} from "../../hooks/ValidationHook/ValidationHook";
 
 function Profile(props) {
     const currentUser = React.useContext(CurrentUserContext);
     const [name, setName] = React.useState('');
     const [email, setEmail] = React.useState('');
+    const [disableButton, setDisableButton] = React.useState(false);
+    const nameValid = useInput('', {isEmpty: false, isName: true});
+    const emailValid = useInput('', {isEmpty: false, isEmail: true});
 
     React.useEffect(() => {
-        setName(currentUser.name);
-        setEmail(currentUser.email);
+        nameValid.value = currentUser.name;
+        emailValid.value = currentUser.email;
     }, [currentUser]);
 
-    function handleChangeName(evt) {
-        setName(evt.target.value);
-    }
+    React.useEffect(() => {
+        setName(nameValid.value)
+    }, [nameValid.value])
 
-    function handleChangeEmail(evt) {
-        setEmail(evt.target.value);
-    }
+    React.useEffect(() => {
+        setEmail(emailValid.value)
+    }, [emailValid.value])
+
+    React.useEffect(() => {
+        console.log(name, currentUser.name, nameValid.value)
+        if (nameValid.value === currentUser.name && emailValid.value === currentUser.email) {
+            setDisableButton(true)
+        } else {
+            setDisableButton(false)
+        }
+    }, [nameValid.value, emailValid.value])
 
     function handleSubmit(evt) {
         evt.preventDefault();
@@ -41,18 +54,22 @@ function Profile(props) {
             <section className="profile">
                 <h1 className="profile__header">Привет, {name}!</h1>
                 <form className="profile__form" onSubmit={handleSubmit}>
+                    {(nameValid.isDirty && nameValid.isEmpty) && <p className="auth__input-error">Поле не может быть пустым</p>}
+                    {(nameValid.isDirty && nameValid.nameError && !nameValid.isEmpty) && <p className="auth__input-error">Данное имя нельзя использовать</p>}
                     <div className="profile__input-container profile__input-container_type_top">
                         <label className="profile__label">Имя</label>
                         <input className="profile__input" name="name" pattern="^[a-zа-яёA-ZА-ЯЁ\-\s/i]+" type="text"
-                               onChange={handleChangeName} value={name || ""}/>
+                               onChange={nameValid.onChange} onBlur={nameValid.onBlur} value={name}/>
                     </div>
+                    {(emailValid.isDirty && emailValid.isEmpty) && <p className="profile__input-error">Поле не может быть пустым</p>}
+                    {(emailValid.isDirty && emailValid.emailError && !emailValid.isEmpty) && <p className="profile__input-error">Введен некорректный email</p>}
                     <div className="profile__input-container profile__input-container_type_bottom">
                         <label className="profile__label">E-mail</label>
                         <input className="profile__input" type="email" name="email"
                                pattern="^([a-zA-Z0-9_-]+\.)*[a-zA-Z0-9_-]+@[a-z0-9_-]+(\.[a-zA-Z0-9_-]+)*\.[a-z]{2,6}$"
-                               onChange={handleChangeEmail} value={email || ""}/>
+                               onChange={emailValid.onChange} onBlur={emailValid.onBlur} value={email}/>
                     </div>
-                    <button className="profile__button">Редактировать</button>
+                    <button className={`profile__button ${(!nameValid.inputValid || !emailValid.inputValid || disableButton) && "profile__button_type_disabled"}`} disabled={!nameValid.inputValid || !emailValid.inputValid || disableButton}>Редактировать</button>
                 </form>
                 <Link to="/" className="profile__link" onClick={logOut}>Выйти из аккаунта</Link>
             </section>
